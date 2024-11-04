15aae1c14477394507322a49ec451deb02eada7b
bihanviranga <bihanviranga@gmail.com>
2024-11-04 16:15:15 +0530

    Proper parsing of redis protocol encoded input

--- app/commands/index.ts
+++ app/commands/index.ts
@@ -3,15 +3,22 @@ import ping from "./ping";
 
 function handleCommand(data: Buffer): string {
   console.log("Data:", data.toString());
-  const dataLowerCase = data.toString().toLowerCase();
+  const splitData = data.toString().split("\r\n");
+  console.log("Split Data", splitData);
+  const length = splitData[0].slice(1);
+  console.log("Length:", length);
+  const commandLength = splitData[1].slice(1);
+  console.log("Command length:", commandLength);
+  const command = splitData[2].toLowerCase();
+  console.log("Command", command);
 
-  if (dataLowerCase.startsWith("ping")) {
+  if (command === "ping") {
     return ping();
   }
 
-  if (dataLowerCase.startsWith("echo")) {
-    return echo(data.toString());
-  }
+  // if (command === "echo") {
+  //   return echo(data.toString());
+  // }
 
   return "Unknown command\r\n";
 }
