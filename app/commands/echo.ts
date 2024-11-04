ea13b24b809c214748a70ad1754b568703ce6b75
bihanviranga <bihanviranga@gmail.com>
2024-11-04 17:01:53 +0530

    Fix return type issue in ECHO

--- app/commands/echo.ts
+++ app/commands/echo.ts
@@ -1,6 +1,7 @@
 export default function (data: Array<string>): string {
   const responseLength = data.length;
-  let response = `*${responseLength}\r\n`;
+  // let response = `*${responseLength}\r\n`;
+  let response = ``;
   data.forEach((item) => {
     const encoded = `\$${item.length}\r\n${item}\r\n`;
     response += encoded;
