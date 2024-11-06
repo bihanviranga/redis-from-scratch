58512bddb939dd69b515e9dea0368eb019adf287
bihanviranga <bihanviranga@gmail.com>
2024-11-07 02:47:04 +0530

    Eyecandy

--- app/server/index.ts
+++ app/server/index.ts
@@ -5,9 +5,8 @@ const PORT = 6379;
 
 export function startServer() {
   const server: net.Server = net.createServer((connection: net.Socket) => {
-    console.log("========================");
     console.log(
-      `Connected: ${connection.remoteAddress}:${connection.remotePort}`,
+      `[server]\tConnected: ${connection.remoteAddress}:${connection.remotePort}`,
     );
 
     connection.on("data", (data: Buffer) => {
@@ -16,15 +15,17 @@ export function startServer() {
     });
 
     connection.on("close", () => {
-      console.log("Connection closed");
+      console.log(`[server]\tConnection closed`);
     });
 
     connection.on("end", () => {
-      console.log("Client disconnected");
+      console.log(
+        `[server]\tDisconnected: ${connection.remoteAddress}:${connection.remotePort}`,
+      );
     });
   });
 
   server.listen(PORT, "127.0.0.1", () => {
-    console.log(`Server listening on ${PORT}`);
+    console.log(`[server]\tServer listening on ${PORT}`);
   });
 }
