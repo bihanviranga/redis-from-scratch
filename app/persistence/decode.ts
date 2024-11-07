db787cff66603b9b97c72a340ae8c616fc133780
bihanviranga <bihanviranga@gmail.com>
2024-11-08 00:15:28 +0530

    Length decode refactoring

---
+++ app/persistence/decode.ts
@@ -0,0 +1,94 @@
+import {
+  RDB_LENGTH_ENCODING_TYPES,
+  RDB_STRING_ENCODING_TYPES,
+} from "../types/persistence";
+
+/**
+ * Decode length-encoded data.
+ * ref: https://rdb.fnordig.de/file_format.html#length-encoding
+ */
+export function decodeLength(
+  buffer: Buffer,
+  startIndex: number,
+): { value: string | number; nextIndex: number } {
+  let decodedValue: string | number;
+  let nextIndex = startIndex;
+
+  const lengthByte = buffer[startIndex];
+  const shiftedByte = lengthByte >> 6;
+  const mask = 0b00000011;
+  const msbs = mask & shiftedByte;
+
+  if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_6_BITS) {
+    decodedValue = lengthByte;
+    nextIndex += 1;
+  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.SPECIAL_ENCODING) {
+    const decodeResult = decodeSpecialEncodedLength(buffer, startIndex);
+    decodedValue = decodeResult.value;
+    nextIndex = decodeResult.nextIndex;
+  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_14_BITS) {
+    throw new Error(
+      "Found length encoding method that is not yet supported in this parser.",
+    );
+  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_4_BYTES) {
+    throw new Error(
+      "Found length encoding method that is not yet supported in this parser.",
+    );
+  } else {
+    throw new Error("Unexpected data found in RDB file. File may be invalid.");
+  }
+
+  return {
+    value: decodedValue,
+    nextIndex,
+  };
+}
+
+/**
+ * Decode lengths that are encoded as strings.
+ * ref: https://rdb.fnordig.de/file_format.html#string-encoding
+ */
+export function decodeSpecialEncodedLength(
+  buffer: Buffer,
+  startIndex: number,
+): { value: number; nextIndex: number } {
+  let nextIndex = startIndex;
+  const lengthByte = buffer[nextIndex];
+  const leastSixBitsMask = 0b00111111;
+  const encodingType = leastSixBitsMask & lengthByte;
+
+  if (encodingType === RDB_STRING_ENCODING_TYPES.INT_8_BIT) {
+    // Value is in the next 8 bits
+    const value = buffer[nextIndex + 1];
+    nextIndex = nextIndex + 1 + 1; // +1 to account for the value, +1 to point to the next byte.
+    return {
+      value,
+      nextIndex,
+    };
+  }
+
+  if (encodingType === RDB_STRING_ENCODING_TYPES.INT_32_BIT) {
+    // Value is in the next 32 bits
+    const value = buffer.readUint32LE(nextIndex + 1);
+    nextIndex = nextIndex + 4 + 1; // +4 to account for the value, +1 to point to the next byte.
+    return {
+      value,
+      nextIndex,
+    };
+  }
+
+  if (encodingType === RDB_STRING_ENCODING_TYPES.INT_16_BIT) {
+    throw new Error(
+      "Found integer encoding method that is not yet supported in this parser.",
+    );
+  }
+
+  if (encodingType === RDB_STRING_ENCODING_TYPES.COMPRESSED_STRING) {
+    throw new Error(
+      "Found integer encoding method that is not yet supported in this parser.",
+    );
+  }
+
+  // Exeecution should not reach here in a valid file.
+  throw new Error("Unexpected data found in RDB file. File may be invalid.");
+}
--- app/persistence/index.ts
+++ app/persistence/index.ts
@@ -8,6 +8,7 @@ import {
   RDB_OP_CODES,
   RDB_STRING_ENCODING_TYPES,
 } from "../types/persistence";
+import { decodeLength, decodeSpecialEncodedLength } from "./decode";
 
 const RDB_VERSION_NUMBER_LENGTH = 4;
 
@@ -161,85 +162,10 @@ function parseMetadata(
 }
 
 function parseDatabaseSelector(buffer: Buffer, startIndex: number): number {
-  let readIndex = startIndex;
-  const lengthByte = buffer[readIndex];
-  const shiftedByte = lengthByte >> 6;
-  const mask = 0b00000011;
-  const msbs = mask & shiftedByte;
-
-  let databaseSelector = 0;
-
-  if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_6_BITS) {
-    databaseSelector = lengthByte;
-    readIndex += 1;
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.SPECIAL_ENCODING) {
-    const decodedLength = decodeSpecialEncodedLength(buffer, readIndex);
-    databaseSelector = decodedLength.value;
-    readIndex = decodedLength.nextIndex;
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_14_BITS) {
-    throw new Error(
-      "Found length encoding method that is not yet supported in this parser.",
-    );
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_4_BYTES) {
-    throw new Error(
-      "Found length encoding method that is not yet supported in this parser.",
-    );
-  } else {
-    throw new Error("Unexpected data found in RDB file. File may be invalid.");
-  }
-
-  // return databaseSelector;
-  return readIndex;
-}
-
-/**
- * Decode lengths that are encoded as strings.
- * ref: https://rdb.fnordig.de/file_format.html#string-encoding
- * TODO: move to a helper file
- */
-function decodeSpecialEncodedLength(
-  buffer: Buffer,
-  startIndex: number,
-): { value: number; nextIndex: number } {
-  let nextIndex = startIndex;
-  const lengthByte = buffer[nextIndex];
-  const leastSixBitsMask = 0b00111111;
-  const encodingType = leastSixBitsMask & lengthByte;
-
-  if (encodingType === RDB_STRING_ENCODING_TYPES.INT_8_BIT) {
-    // Value is in the next 8 bits
-    const value = buffer[nextIndex + 1];
-    nextIndex = nextIndex + 1 + 1; // +1 to account for the value, +1 to point to the next byte.
-    return {
-      value,
-      nextIndex,
-    };
-  }
-
-  if (encodingType === RDB_STRING_ENCODING_TYPES.INT_32_BIT) {
-    // Value is in the next 32 bits
-    const value = buffer.readUint32LE(nextIndex + 1);
-    nextIndex = nextIndex + 4 + 1; // +4 to account for the value, +1 to point to the next byte.
-    return {
-      value,
-      nextIndex,
-    };
-  }
-
-  if (encodingType === RDB_STRING_ENCODING_TYPES.INT_16_BIT) {
-    throw new Error(
-      "Found integer encoding method that is not yet supported in this parser.",
-    );
-  }
-
-  if (encodingType === RDB_STRING_ENCODING_TYPES.COMPRESSED_STRING) {
-    throw new Error(
-      "Found integer encoding method that is not yet supported in this parser.",
-    );
-  }
-
-  // Exeecution should not reach here in a valid file.
-  throw new Error("Unexpected data found in RDB file. File may be invalid.");
+  const lengthResult = decodeLength(buffer, startIndex);
+  const databaseSelector = lengthResult.value;
+  const nextIndex = lengthResult.nextIndex;
+  return nextIndex;
 }
 
 /*
@@ -252,9 +178,11 @@ function parseExpiryTime(buffer: Buffer, startIndex: number): number {
   let nextIndex = startIndex;
 
   if (byte === RDB_OP_CODES.EXPIRETIME) {
+    // Expire time is in seconds, stored in the next 4 bytes.
     value = buffer.readUint32LE(startIndex + 1);
     nextIndex = startIndex + 4 + 1; // +4 to account for the value, +1 to point to the next byte.
   } else {
+    // Expire time is in milliseconds, stored in the next 8 bytes.
     value = buffer.readBigUInt64LE(startIndex + 1);
     nextIndex = startIndex + 8 + 1; // +8 to account for the value, +1 to point to the next byte.
   }
@@ -263,58 +191,19 @@ function parseExpiryTime(buffer: Buffer, startIndex: number): number {
   return nextIndex;
 }
 
+/*
+ * Parse the resizedb values.
+ */
 function parseResizedb(buffer: Buffer, startIndex: number): number {
   let nextIndex = startIndex;
-  let lengthByte = buffer[nextIndex];
-  let shiftedByte = lengthByte >> 6;
-  const mask = 0b00000011;
-  let msbs = mask & shiftedByte;
 
-  let hashTableSize = 0;
+  const hashTableSizeResult = decodeLength(buffer, nextIndex);
+  const hashTableSize = hashTableSizeResult.value;
+  nextIndex = hashTableSizeResult.nextIndex;
 
-  if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_6_BITS) {
-    hashTableSize = lengthByte;
-    nextIndex += 1;
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.SPECIAL_ENCODING) {
-    const decodedLength = decodeSpecialEncodedLength(buffer, nextIndex);
-    hashTableSize = decodedLength.value;
-    nextIndex = decodedLength.nextIndex;
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_14_BITS) {
-    throw new Error(
-      "Found length encoding method that is not yet supported in this parser.",
-    );
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_4_BYTES) {
-    throw new Error(
-      "Found length encoding method that is not yet supported in this parser.",
-    );
-  } else {
-    throw new Error("Unexpected data found in RDB file. File may be invalid.");
-  }
-
-  let expiryTableSize = 0;
-
-  lengthByte = buffer[nextIndex];
-  shiftedByte = lengthByte >> 6;
-  msbs = mask & shiftedByte;
-
-  if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_6_BITS) {
-    expiryTableSize = lengthByte;
-    nextIndex += 1;
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.SPECIAL_ENCODING) {
-    const decodedLength = decodeSpecialEncodedLength(buffer, nextIndex);
-    expiryTableSize = decodedLength.value;
-    nextIndex = decodedLength.nextIndex;
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_14_BITS) {
-    throw new Error(
-      "Found length encoding method that is not yet supported in this parser.",
-    );
-  } else if (msbs === RDB_LENGTH_ENCODING_TYPES.READ_4_BYTES) {
-    throw new Error(
-      "Found length encoding method that is not yet supported in this parser.",
-    );
-  } else {
-    throw new Error("Unexpected data found in RDB file. File may be invalid.");
-  }
+  const expiryTableSizeResult = decodeLength(buffer, nextIndex);
+  const expiryTableSize = expiryTableSizeResult.value;
+  nextIndex = expiryTableSizeResult.nextIndex;
 
   console.log("hashTableSize:", hashTableSize);
   console.log("expiryTableSize", expiryTableSize);
