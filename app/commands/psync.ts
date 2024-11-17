ab0715a5b1dceda3a8a6a0572d449399ebcaa0a6
bihanviranga <bihanviranga@gmail.com>
2024-11-17 23:32:10 +0530

    Trying a random approach to send bytes now

--- app/commands/psync.ts
+++ app/commands/psync.ts
@@ -10,8 +10,22 @@ export default function (data: Array<string>, connection: net.Socket) {
   const responseToEncode = `${FULLRESYNC} ${replId} ${replOffset}`;
   const encoded = encodeSimpleString(responseToEncode);
   connection.write(encoded);
-  const rdbPayload = `$${EMPTY_RDB_FILE.length}\r\n${EMPTY_RDB_FILE}`;
-  connection.write(rdbPayload);
+
+  // const byteArray = new Uint8Array(EMPTY_RDB_FILE.length / 2);
+  // for (let i = 0; i < EMPTY_RDB_FILE.length; i += 2) {
+  //   byteArray[i / 2] = parseInt(EMPTY_RDB_FILE.substr(i, 2), 16);
+  // }
+  const base64 =
+    "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
+
+  const rdbBuffer = Buffer.from(base64, "base64");
+
+  const rdbHead = Buffer.from(`$${rdbBuffer.length}\r\n`);
+
+  console.log(rdbHead.toString() + rdbBuffer.toString());
+  // connection.write(Buffer.concat([rdbHead, rdbBuffer]));
+  // const rdbPayload = `$${byteArray.length}\r\n${byteArray.toString()}`;
+  connection.write(rdbHead.toString() + rdbBuffer.toString());
   // Adding a fake timeout because when we send the data at once sometimes it gets considered as a single call.
   // setTimeout(() => {
   //   const rdbPayload = `$${EMPTY_RDB_FILE.length}\r\n${EMPTY_RDB_FILE}`;
